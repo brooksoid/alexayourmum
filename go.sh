8ee6a711afa40ee8bd6547f0a3f4fac92f6f7d14
Matthew Brooks <matthew.brooks@bbc.co.uk>
2017-11-29 15:10:25 +0000

    Update-function-code go.sh

--- go.sh
+++ go.sh
@@ -3,15 +3,12 @@ pushd src
 zip -r ../src.zip *
 popd
 aws lambda update-function-code --function-name alexaYourMumSkill --zip-file fileb://src.zip
+
 #aws s3 cp src.zip s3://matthewsalexa/
 
-
-#https://developer.amazon.com/blogs/post/Tx1UE9W1NQ0GYII/Publishing-Your-Skill-Code-to-Lambda-via-the-Command-Line-Interface
-
-
-#aws lambda update-function-code --function-name MyLambdaFunction --zip-file fileb://index.zip
-
-
+# https://developer.amazon.com/blogs/post/Tx1UE9W1NQ0GYII/Publishing-Your-Skill-Code-to-Lambda-via-the-Command-Line-Interface
+# use AWS configure to create credentials
+# aws lambda update-function-code --function-name MyLambdaFunction --zip-file fileb://index.zip
 
 #$ aws lambda update-function-configuration \
 #   --function-name CreateThumbnail  \
